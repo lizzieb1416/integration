a2613a1b3ed28c3320356c54b893fc8ce3999508
lisetTablet <lizethbastidas@gmail.com>
2020-08-29 09:57:21 +0200

    Creation of venv in VSC and in pi

--- server/server.py
+++ server/server.py
@@ -1,4 +1,11 @@
 import socket
+import pylogging
+import os
+
+# Logs Dir Absolute Path
+logs_path = os.path.dirname(os.path.abspath(__file__)) + '/logs/'
+# Create Logger Instance
+logger = pylogging.PyLogging(LOG_FILE_PATH = logs_path)
 
 hoste = ''
 port = 12800
@@ -14,6 +21,7 @@ msg_received = b""
 while msg_received != b"fin":
     msg_received = connection_with_client.recv(1024)
     print(msg_received.decode())
+    logger.log("Message received : {}".format(msg_received.decode()))
     connection_with_client.send(b"5 / 5")
 
 print("Closing the connection")
